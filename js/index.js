31511b9d25e21444872f8ba135b88c3ec9e9ee77
Salma-Yahya24 <salmayahya1812@gmail.com>
2024-11-03 20:28:15 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        var typed = new Typed('#typed-output', {
+            strings: ["Developer", "Larry Daniels","Designer"],
+            typeSpeed: 70,    // سرعة الكتابة (بالمللي ثانية)
+            backSpeed: 50,    // سرعة المسح (بالمللي ثانية)
+            loop: true        // إذا كنت تريد التكرار
+        });
+    });
+
